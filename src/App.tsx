4103cba7f09b2f09ac85c645b9e6112e77bb5f4e
Charlotte Liston <yaihilbeskar@gmail.com>
2022-02-10 13:02:21 -0500

    HTML/CSS tasks first blush

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -7,11 +8,37 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>My Name is Charlotte Liston. Hello World!</p>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <Container>
+                <Row>
+                    <Col>
+                        <img
+                            className="App-image-me"
+                            src="https://i.imgur.com/X39E2WM.png"
+                            alt="Some art of myself, drawn by my fiance."
+                        />
+                        <div className="App-col-box"></div>
+                    </Col>
+                    <Col>
+                        <div className="App-col-box">
+                            <p>My Name is Charlotte Liston. Hello World!</p>
+                            <ul>
+                                <li>
+                                    I am a senior Cognitive Science major at UD
+                                </li>
+                                <li>I&aposm a trans woman</li>
+                                <li>I am engaged</li>
+                            </ul>
+                            <p>
+                                Edit <code>src/App.tsx</code> and save. This
+                                page will automatically reload.
+                            </p>
+                            <Button onClick={() => console.log("Hello World!")}>
+                                Log Hello World
+                            </Button>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
