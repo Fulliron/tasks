a9229c2b341c6a70594c1fd57fcea79d28664433
Charlotte Liston <yaihilbeskar@gmail.com>
2022-04-03 01:50:23 -0400

    completed stringsToIntegers

--- src/arrays.ts
+++ src/arrays.ts
@@ -30,7 +30,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const numbersFromString = numbers.map((num: string): number =>
+        parseInt(num) > 0 || parseInt(num) <= 0 ? parseInt(num) : 0
+    );
+    return numbersFromString;
 }
 
 /**
